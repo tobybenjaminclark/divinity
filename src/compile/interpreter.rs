bd00369b4985a49578290779cc2a8fbb237d665f
aswarbs <amberswarbrick1@gmail.com>
2024-11-16 14:05:02 +0000

    change some interpreter function stuff

--- src/compile/interpreter.rs
+++ src/compile/interpreter.rs
@@ -5,7 +5,7 @@ use crate::ast::{Block, Expr, Opcode, Program, Statement, TypedArgument};
 
 #[derive(Debug, Clone)]
 enum Var {
-    Int(i64),
+    Int(i32),
     Float(f64)
 }
 type SymbolTable = HashMap<String, Var>;
@@ -21,7 +21,7 @@ pub fn evaluate_program(program: Program) -> Box<dyn Any> {
 
     // get main
     if let Some(declaration) = functions.get("main") {
-        let ret = evaluate_block(declaration, &functions);
+        let ret = evaluate_block(declaration, symbol_table);
         return Box::from(ret);
     }
 
@@ -29,25 +29,40 @@ pub fn evaluate_program(program: Program) -> Box<dyn Any> {
     return Box::from(1);
 }
 
-// evaluate function blocks
-fn evaluate_block(block: &Block, functions: &HashMap<String, Block>) {
+// evaluate blocks
+fn evaluate_block(block: &Block, symbol_table: SymbolTable) {
 
     match block {
         Block::FunctionDefinition(name, vars, ret, body) => {
-
-            // create a symbol table with vars
-            let mut symbol_table: SymbolTable = HashMap::new();
-            for var in vars {
-                if let TypedArgument::TypedArgument(name, _) = var {
-                    symbol_table.insert(String::from(name), Var::Int(0));
-                }
-            }
-            println!("symbol table: {:?}", &symbol_table);
-
-            // evaluate the body with evaluate_function_body(body, symbol table)
+            evaluate_function(block, &vec![], symbol_table)
         }
         Block::TypeDefinition(_, _, _) => {}
     }
+}
+
+fn evaluate_function(function: &Block, args: &Vec<&Expr>, symbol_table: SymbolTable) {
+        // create a symbol table with vars
+        let mut symbol_table: SymbolTable = HashMap::new();
+
+        match function {
+            Block::FunctionDefinition(name, params, ret, body) => {
+                if args.len() != params.len() {panic!("args and params have different lengths")}
+
+                for (index, param) in params.iter().enumerate() {
+                    match param {
+                        TypedArgument::TypedArgument(name, typ) => {
+                            symbol_table.insert(name.to_string(), evaluate_expression(args[index]));
+                        }
+                    }
+                }
+
+                for var in args {
+                    // insert the called var value in its corresponding position in the symbol table
+                    symbol_table.insert(String::from(name), evaluate_expression(var));
+                }
+            }
+            _ => {panic!("expected function")}
+        }
 }
 
 fn evaluate_function_body(body: &Vec<Statement>, symbol_table: &SymbolTable) {
@@ -65,13 +80,13 @@ fn evaluate_statement(statement: &Statement, symbol_table: &SymbolTable) {
     }
 }
 
-fn evaluate_expression(expr: &Expr) {
+fn evaluate_expression(expr: &Expr) -> Var {
     match expr {
-        Expr::Number(n) => {}
-        Expr::Op(l, op, r) => {}
-        Expr::FunctionCall(name, params) => {}
-        Expr::Conditional(i, t, e) => {}
-        Expr::Identifier(name) => {}
+        Expr::Number(n) => {Var::Int(*n)}
+        Expr::Op(l, op, r) => {Var::Int(0)}
+        Expr::FunctionCall(name, params) => {Var::Int(0)}
+        Expr::Conditional(i, t, e) => {Var::Int(0)}
+        Expr::Identifier(name) => {Var::Int(0)}
     }
 }
 
